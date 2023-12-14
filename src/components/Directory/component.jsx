115cc11c781738da5165291b64a9f3c2af8a3d41
insanetim <47120724+insanetim@users.noreply.github.com>
2023-12-14 19:17:18 +0200

    updating our directory with directory component and category-item component

---
+++ src/components/Directory/component.jsx
@@ -0,0 +1,18 @@
+import CategoryItem from '../CategoryItem'
+
+import './styles.scss'
+
+const Directory = ({ categories }) => {
+  return (
+    <div className='categories-container'>
+      {categories.map(category => (
+        <CategoryItem
+          key={category.id}
+          category={category}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Directory
