e03d6e398ab53403c919b7f4143b5e8270d12ca2
insanetim <47120724+insanetim@users.noreply.github.com>
2023-12-22 11:37:57 +0200

    added redux-persist and redux devtools

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import { store } from './store'
 import App from './App'
+import { store, persistor } from './store'
+
 import './index.scss'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate
+      persistor={persistor}
+      loading={null}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 )
